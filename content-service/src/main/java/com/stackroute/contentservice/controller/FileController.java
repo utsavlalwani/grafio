0ec6149d0f3cf3dd41187a55decd5b3c67c2796f
saptarshi <saptarshi.majumdar@cgi.com>
2019-10-28 13:02:18 +0530

    added aws access to content-service

--- content-service/src/main/java/com/stackroute/contentservice/controller/FileController.java
+++ content-service/src/main/java/com/stackroute/contentservice/controller/FileController.java
@@ -1,24 +1,12 @@
 package com.stackroute.contentservice.controller;
 
-import com.stackroute.contentservice.domain.File;
-import com.stackroute.contentservice.service.FileStorageService;
+import com.stackroute.contentservice.service.AmazonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -27,54 +15,22 @@ public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
+    private AmazonClient amazonClient;
+
     @Autowired
-    private FileStorageService fileStorageService;
+    public FileController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
 
     @PostMapping("/file")
-    public File uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
-
-        String fileDownloadUri = "http://localhost:8080/content-service/api/v1/file/" + fileName;
-
-        return new File(fileName, fileDownloadUri,
-                file.getContentType(), file.getSize());
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
     }
 
-    @PostMapping("/files")
-    public List<File> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
-        return Arrays.asList(files)
-                .stream()
-                .map(file -> uploadFile(file))
-                .collect(Collectors.toList());
-    }
 
-    @GetMapping("/file/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = null;
-        try {
-            resource = fileStorageService.loadFileAsResource(fileName);
-        } catch (FileNotFoundException e) {
-            logger.info("File not found: " + e.getMessage());
-        }
-
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+    @DeleteMapping("/file")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
     }
 
 }
