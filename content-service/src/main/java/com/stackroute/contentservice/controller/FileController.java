08c2fb26be74d59b22dc6e767ccc0f5fa0b68cd3
saptarshi majumdar <saptarshi.majumdar>
2019-10-31 12:49:38 +0530

    .

--- content-service/src/main/java/com/stackroute/contentservice/controller/FileController.java
+++ content-service/src/main/java/com/stackroute/contentservice/controller/FileController.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,8 +25,8 @@ public class FileController {
     }
 
     @PostMapping("/file")
-    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
-        return this.amazonClient.uploadFile(file);
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return new ResponseEntity<String>(this.amazonClient.uploadFile(file), HttpStatus.CREATED);
     }
 
 
