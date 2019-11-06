d84fb85bb41e3cf276c78ed79b81f334bbe2048f
saptarshi majumdar <saptarshi.majumdar>
2019-11-06 23:02:49 +0530

    .

--- api-gateway/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
+++ api-gateway/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
@@ -1,9 +1,11 @@
 package com.stackroute.zuulapi;
 
+import com.stackroute.zuulapi.filters.Prefilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +14,10 @@ public class ZuulApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiApplication.class, args);
+	}
+
+	@Bean
+	public Prefilter prefilter() {
+		return new Prefilter();
 	}
 }
