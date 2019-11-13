7dbc4c4dcb42c1113ade83520bd2ed233ef8e6d1
saptarshi majumdar <saptarshi.majumdar>
2019-11-13 17:10:10 +0530

    .

--- frontend/src/app/trending/trending.component.ts
+++ frontend/src/app/trending/trending.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Router} from '@angular/router';
+import {PostedService} from '../services/posted.service';
 
 @Component({
   selector: 'app-trending',
@@ -11,8 +12,9 @@ import {Router} from '@angular/router';
 export class TrendingComponent implements OnInit {
 
   posts: any;
-  constructor(private http: HttpClient,
-              private router: Router) { }
+  breakpoint: number;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     const httpOptions = {
@@ -24,11 +26,17 @@ export class TrendingComponent implements OnInit {
     };
     this.http.get(environment.uploadPostUrl + 'trending', httpOptions).subscribe(
       (data) => {
-        this.posts = data['posts'];
-      }/*, (error) => {
-        this.router.navigateByUrl('/404');
-      }*/
-    );
+        this.posts = data;
+
+      this.breakpoint = (window.innerWidth <= 600) ? 1:(window.innerWidth <= 959 && window.innerWidth > 600 )
+        ? 2:(window.innerWidth <= 1279 && window.innerWidth > 959 ) ? 3:(window.innerWidth <= 1919
+          && window.innerWidth > 1279 ) ? 4:5;;
+    });
   }
 
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 600) ? 1 : (window.innerWidth <= 959 && window.innerWidth > 600 )
+      ? 2:(window.innerWidth <= 1279 && window.innerWidth > 959 ) ? 3:(window.innerWidth <= 1919
+        && window.innerWidth > 1279 ) ? 4:5;;
+  }
 }
