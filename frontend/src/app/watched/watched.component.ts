ce4797ce11370c4a940754e58dffcd1b296cbe3f
surjashish.sengupta <surjashish.sengupta@cgi.com>
2019-11-18 14:23:40 +0530

    flag added

--- frontend/src/app/watched/watched.component.ts
+++ frontend/src/app/watched/watched.component.ts
@@ -17,16 +17,14 @@ export class WatchedComponent implements OnInit {
     this.posted.getPosts().subscribe(data => {
       this.posts = data['watched'];
 
-      this.breakpoint = (window.innerWidth <= 600) ? 1:(window.innerWidth <= 959 && window.innerWidth > 600 )
-        ? 2:(window.innerWidth <= 1279 && window.innerWidth > 959 ) ? 3:(window.innerWidth <= 1919
-          && window.innerWidth > 1279 ) ? 4:5;;
+      this.breakpoint = (window.innerWidth <= 777) ? 1:(window.innerWidth <= 1120 && window.innerWidth > 777 )
+      ? 2:( window.innerWidth > 1120 ) ? 3:4;
     });
   }
 
   onResize(event) {
-    this.breakpoint = (event.target.innerWidth <= 600) ? 1 : (window.innerWidth <= 959 && window.innerWidth > 600 )
-      ? 2:(window.innerWidth <= 1279 && window.innerWidth > 959 ) ? 3:(window.innerWidth <= 1919
-        && window.innerWidth > 1279 ) ? 4:5;;
+    this.breakpoint = (window.innerWidth <= 777) ? 1:(window.innerWidth <= 1120 && window.innerWidth > 777 )
+    ? 2:( window.innerWidth > 1120 ) ? 3:4;
   }
 
 }
